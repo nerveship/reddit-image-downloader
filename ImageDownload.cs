95b71e41cbf707c22754bcc0718aa6c599ed57bd
John-Stuart Gonsales <johnstuartgonsales@gmail.com>
2023-02-21 01:50:55 +0000

    added check for i.redd.it

--- ImageDownload.cs
+++ ImageDownload.cs
@@ -1,15 +1,33 @@
-﻿namespace reddit_image_downloader
+﻿using HtmlAgilityPack;
+using System.Linq;
+
+namespace reddit_image_downloader
 {
     internal class ImageDownload
     {
         internal void SubredditDownload()
         {
-            Console.WriteLine("Subreddit download page");
+            Console.WriteLine("Which subreddit would you like to download images from?");
+            string UserChoice = Console.ReadLine();
         }
 
         internal void UserDownload()
         {
-            Console.WriteLine("User download page");
+            Console.Clear();
+            Console.WriteLine("Which user would you like to download images from?");
+
+            string UserChoice = Console.ReadLine();
+            var TargetUrl = new HtmlWeb().Load($"https://www.reddit.com/u/{UserChoice}");
+
+            var ImageUrls = TargetUrl.DocumentNode.Descendants("img")
+                .Select(i => i.GetAttributeValue("src", ""))
+                .Where(e => e.Contains("i.redd.it"));
+
+            //For each image that contains i.reddit, print the link
+            foreach (string image in ImageUrls)
+            {
+                Console.WriteLine(image);
+            }
         }
     }
 }
